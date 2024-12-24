95dcbb3c4adc77e170584488b5b5b5fcac697a65
Harshada Palanikkumar <hpalanik@ucsc.edu>
2024-12-23 17:45:04 -0800

    Initial Commit

---
+++ Harshada's First Swift Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Harshada's First Swift Project
+//
+//  Created by Harshada Palanikkumar on 12/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
